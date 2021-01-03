d0a7e1991f2703758f8a3d6f5c3b65914097f4cf
Tihomir10 <36307628+Tihomir10@users.noreply.github.com>
2021-01-03 19:49:40 +0100

    refactor

--- src/components/education/Education.js
+++ src/components/education/Education.js
@@ -4,9 +4,7 @@ import EducationInfo from './EducationInfo';
 import EducationForm from './EducationForm';
 
 function Education() {
-  const [ education, setEducation ] = useState([{id: 0, school: 'University of Berlin', title: 'Master of None', date: '2011 - 2014'}, {
-    id: 1, school: 'University of Lisbon', title: 'Alpinist', date: '2004 - 2013'
-  }]);
+  const [ education, setEducation ] = useState([{id: 0, school: 'University of Berlin', title: 'Master of None', date: '2011 - 2014'}]);
 
   const [ school, setSchool ] = useState('');
 
@@ -14,19 +12,40 @@ function Education() {
 
   const [ date, setDate ] = useState('');
 
-  const [ edit, setEdit ] = useState(false);
+  const [ newEducation, setNewEducation ] = useState(false);
 
-  const [ newEducation, setNewEducation ] = useState(false)
+  const [ schoolError, setSchoolError ] = useState('');
 
-  function updateEducationData(event) {
-    setEducation([...education,{
-      id: education.length,
-      school: school,
-      title: title,
-      date: date
-    }]);
-    setNewEducation(false)
-    event.preventDefault()
+  const [ titleError, setTitleError ] = useState('');
+
+  const validate = () => {
+    if (school.length < 3) {
+      setSchoolError('Must be longer than 3 characters')
+      return false;
+    }
+
+    if (title.length < 3) {
+      setTitleError('Must be longer than 3 characters')
+      return false;
+    }
+
+    return true;
+  }
+
+  function updateEducationData() {
+    const isValid = validate();
+    if (isValid) {
+      setEducation([...education,{
+        id: education.length,
+        school: school,
+        title: title,
+        date: date
+      }]);
+      setNewEducation(false);
+      setSchoolError('');
+      setTitleError('')
+    }
+    
   } 
 
   function handleSchoolChange(event) {
@@ -42,44 +61,36 @@ function Education() {
   }
 
   function deleteEducation(event) {
-    console.log(event.target.id)
     setEducation(education.filter(obj => obj.id != event.target.id))
-  }
-
-  if (edit) {
-    return (
-      <div className='info-edit'>
-        <h3>Education Form</h3>
-        {education.map(obj => {
-          return (
-            <div> 
-                <EducationInfo 
-                  obj={obj}
-                />
-              <div className='delete-btn-div'>
-                 <button className='btn btn-primary' id={obj.id} onClick={deleteEducation}>Delete</button>
-              </div>
-            </div>
-          )
-        })}
-          <button className='btn btn-primary' onClick={() => {setEdit(false)}}>Submit</button>
-      </div>
-    )
   }
 
   if (newEducation) {
     return (
-      <div className='info-edit'>
-        <h3>Education Form</h3>
+      <div>
+        <div>
+          <h3>Education Info</h3>
+            {education.map(obj => {
+            return (
+              <div>
+                <EducationInfo 
+                  obj={obj} 
+                  deleteEducation={deleteEducation}  
+                />
+              </div>
+            )
+          })}
+        </div>
         <EducationForm 
           school={school}
+          schoolError={schoolError}
           title={title}
+          titleError={titleError}
           date={date}
           handleSchoolChange={handleSchoolChange}
           handleTitleChange={handleTitleChange}
           handleDateChange={handleDateChange}
+          updateEducationData={updateEducationData}
         />
-        <button className='btn btn-primary' onClick={updateEducationData}>Submit</button>
       </div>
     )
   }
@@ -90,12 +101,14 @@ function Education() {
         {education.map(obj => {
         return (
           <div>
-            <EducationInfo obj={obj} />
+            <EducationInfo 
+              obj={obj} 
+              deleteEducation={deleteEducation}  
+            />
           </div>
         )
       })}
       <div className='buttons'>
-        <button className='btn btn-primary' onClick={() => {setEdit(true)}}>Edit</button>
         <button className='btn btn-primary' onClick={() => {setNewEducation(true)}}>Add Education</button>
       </div>
     </div>
--- src/components/education/EducationForm.js
+++ src/components/education/EducationForm.js
@@ -1,23 +1,29 @@
 function EducationForm(props) {
   return (
-    <div className="container">
-      <div>
-        <form>
-          <div className="row">
-            <label>School:<br/>
-              <input type="text" name="school" value={props.school} onChange={props.handleSchoolChange} />
-            </label>
-            <label>Title:<br/>
-              <input type="text" name="title" value={props.title} onChange={props.handleTitleChange} />
-            </label>
-          </div>
-          <div className="row">
-            <label>Date:<br/>
-              <input type="text" name="date" value={props.date} onChange={props.handleDateChange} />
-            </label>
-          </div>
-        </form>
+    <div className='edu-edit'>
+      <h3>Education Form</h3>
+      <div className="container">
+        <div>
+          <form>
+            <div className="row">
+              <label>School:<br/>
+                <input type="text" name="school" value={props.school} onChange={props.handleSchoolChange} />
+                <div class='err-msg'>{props.schoolError}</div>
+              </label>
+              <label>Title:<br/>
+                <input type="text" name="title" value={props.title} onChange={props.handleTitleChange} />
+                <div class='err-msg'>{props.titleError}</div>
+              </label>
+            </div>
+            <div className="row">
+              <label>Date:<br/>
+                <input type="text" name="date" value={props.date} onChange={props.handleDateChange} />
+              </label>
+            </div>
+          </form>
+        </div>
       </div>
+      <button className='btn btn-primary' onClick={props.updateEducationData}>Submit</button>
     </div>
   )
 }
